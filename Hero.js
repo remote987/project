60aaee49cd533644f3bd94224a4836074fa9d33e
remote987 <abhisheksasalatti@gmail.com>
2024-09-27 16:01:01 +0530

    Update Hero.js

---
+++ Hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+function Hero() {
+    return ( 
+       <div className='container'>
+        <div className='row'>
+            <img 
+            src="media/images/homeHero.png" alt=" "
+            />
+        </div>
+       </div>
+    // <h1>hero <i class="fa fa-user-circle"aria-hidden="true"></i> </h1>
+     );
+}
+
+export default Hero ;
